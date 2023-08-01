5edfa614077b50900a346db18a261c280ebdb018
Javis <dai@mobilefolk.com>
2023-08-01 17:39:42 +0700

    SAT-13 update docker for report

--- RunAllureReport.sh
+++ RunAllureReport.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
-echo "------ Step 01: Set project path to variable -------"
-project_path="/Users/Automation Testing/Automation_Testing_MBF/mbf_framework_showslinger_web"
+
+echo "------ Step 01: Set current project path to variable -------"
+project_path=$(pwd)
+
 echo "------ Step 02: Go to project path folder -------"
 cd "$project_path"
+
 echo "------ Step 03: Run the testcases -------"
 java -javaagent:"$project_path/libAllureReport/aspectjweaver-1.9.8.jar" -classpath "$project_path/bin:$project_path/libAllureReport/*:$project_path/libLog4J/*:$project_path/lib/*:$project_path/libReportNG/*:$project_path/libWebDriverManager/*:$project_path/libSelenium/*" org.testng.TestNG "$project_path/bin/runShowSlingerTest.xml"
+
 echo "------ Step 04: Load allure command line setting -------"
 source ~/.bash_profile
-echo "------ Step 05: Generate Allure HTML Report -------"
-allure serve allure-results --port 4000
+
+echo "------ Step 05: Generate Allure XML Report -------"
+allure generate allure-results -o "$project_path/allure-report" --clean
+
+echo "------ Step 06: Store Allure Report in a Directory -------"
+current_date=$(date '+%Y-%m-%d-%H-%M-%S')
+report_directory="$project_path/reports/$current_date"
+mkdir -p "$report_directory"
+cp -R "$project_path/allure-report" "$report_directory"
---
+++ dockerfile
@@ -0,0 +1,8 @@
+# Use the official Nginx base image
+FROM nginx
+
+# Copy the custom Nginx configuration file to the container
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+COPY reports /usr/share/reports
+USER root
+RUN chown -R nginx:nginx /usr/share/reports
