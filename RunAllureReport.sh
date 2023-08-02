464104cebe044d4a023af2bbba81ca3273ad8f37
Javis <dai@mobilefolk.com>
2023-08-02 12:22:06 +0700

    SAT-13 update report sh file

--- RunAllureReport.sh
+++ RunAllureReport.sh
@@ -7,7 +7,7 @@ echo "------ Step 02: Go to project path folder -------"
 cd "$project_path"
 
 echo "------ Step 03: Run the testcases -------"
-java -javaagent:"$project_path/aspectjweaver-1.9.8.jar" -classpath "$project_path/bin:$project_path/libAllureReport/*:$project_path/libLog4J/*:$project_path/lib/*:$project_path/libReportNG/*:$project_path/libWebDriverManager/*:$project_path/libSelenium/*" org.testng.TestNG "$project_path/bin/runShowSlingerTest.xml"
+java -javaagent:"$project_path/libAllureReport/aspectjweaver-1.9.8.jar" -classpath "$project_path/bin:$project_path/libAllureReport/*:$project_path/libLog4J/*:$project_path/lib/*:$project_path/libReportNG/*:$project_path/libWebDriverManager/*:$project_path/libSelenium/*" org.testng.TestNG "$project_path/bin/runShowSlingerTest.xml"
 
 echo "------ Step 04: Load allure command line setting -------"
 source ~/.bash_profile
@@ -16,7 +16,7 @@ echo "------ Step 05: Generate Allure XML Report -------"
 allure generate allure-results -o "$project_path/allure-report" --clean
 
 echo "------ Step 06: Store Allure Report in a Directory -------"
-current_date="showslinger-web-report-$(date '+%Y-%m-%d-%H-%M-%S')"
+current_date=$(date '+%Y-%m-%d-%H-%M-%S')
 report_directory="$project_path/reports/$current_date"
 mkdir -p "$report_directory"
 cp -R "$project_path/allure-report" "$report_directory"
