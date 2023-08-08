11b6174669067827b688a2468489c70fb1a7d464
Giang Dang <dangthigiang@mobilefolk.com>
2023-08-08 16:06:27 +0700

    SAT-13 update Class name

---
+++ src/test/java/showslinger_user/Class_001_Actions_Of_Event.java
@@ -0,0 +1,465 @@
+package showslinger_user;
+
+import commons.BaseTest;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import pageObject.user.UserActionOfEventPageObject;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.util.Random;
+
+import static org.testng.Assert.assertFalse;
+
+//Actions of tickets Create/ Edit/Transfer Ticket
+public class Class_001_Actions_Of_Event extends BaseTest{
+	private UserActionOfEventPageObject eventPage;
+	private String emailManager, passwordManager;
+	private String dayStart, dayEnd, dayEndInValid,eventName,eventNameNew, dayStartAfter, dayEndAfter, dayStartCopy;
+	private String fullName, phone, validEmail;
+	private String cardNumberValid, monthYearValid, cvc, zip;
+	private String ticketName, quantity;
+	
+	@Parameters({ "envName", "serverName", "browser", "ipAddress", "portNumber", "osName", "osVersion"})
+	@BeforeClass
+	public void beforeClass(@Optional("local") String envName, @Optional("dev") String serverName,
+	@Optional("chrome") String browserName, @Optional("localhost") String ipAddress, @Optional("4444") String
+	portNumber,@Optional("Windows") String osName, @Optional("10") String osVersion) {
+		driver = getBrowserDriver(envName, serverName, browserName, ipAddress, portNumber, osName, osVersion);
+		eventPage = new UserActionOfEventPageObject(driver);
+		
+		emailManager ="paulv@showslinger.com";
+		passwordManager = "12345";
+		dayStart = "12/07/2023";
+		dayEnd = "30/07/2023";
+		dayEndInValid = "12/06/2023";
+		eventName = "Test" + " "+ dayStart +" "+generateFakeNumber();
+		eventNameNew = "Test"+ " " +generateFakeNumber();
+
+		dayStartCopy ="15/07/2023";
+		dayStartAfter ="15/07/2023";
+		dayEndAfter = "30/07/2023";
+		
+		fullName = "Dang Thi Giang";
+		phone = "+128379292999";
+		validEmail = "dangthigiang+2@mobilefolk.com";
+
+		ticketName ="Vip 1";
+		quantity ="1";
+		
+		cardNumberValid = "4242424242424242";
+		monthYearValid = "0424";
+		cvc = "242";
+		zip = "42424";
+	}
+
+	@Description("Login Account")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 1)
+	public void Ticket_OpenUrlAndLoginAccount() {
+		
+		eventPage.clickToLoginLink();
+
+		eventPage.loginAccount(emailManager,passwordManager);
+
+		eventPage.clickToLoginButton();
+	}
+	
+	@Description("Create Ticket - Fail")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 2)
+	public void CreateTicket_001_CreateTicketFail() {
+		eventPage.clickToCreateTicketButton();
+		
+		assertTrue(eventPage.isNameOfPopupDisplayed("Create Ticket"));
+		
+		eventPage.clickToCommitButton("Create Ticket");
+		assertEquals(eventPage.getErrorMessageAtFooterPopup(), "You must select a date and time before adding an act!");
+		
+		eventPage.inputValueTimesDay("Day start", dayStart);
+		eventPage.inputValueTimesDay("Day end", dayEndInValid);
+		eventPage.clickToCommitButton("Create Ticket");
+		eventPage.clickToLink("Cancel");
+		
+		eventPage.clickToCreateTicketButton();
+		eventPage.inputValueTimesDay("Day start", dayStart);
+		eventPage.inputValueTimesDay("Day end", dayEnd);
+	
+		eventPage.clickToCommitButton("Create Ticket");
+		assertEquals(eventPage.getErrorMessageAtFooterPopup(), "You must create at least 1 ticket level. Click the blue \"Add ticket\" button.");
+		
+		eventPage.clickToAddButton("Add Ticket");
+		eventPage.clickToCommitButton("Create Ticket");
+		eventPage.sleepInSecond(5);
+		assertEquals(eventPage.getErrorMessageAtFooterPopup(), "Please create a ticket level by clicking \"Add ticket\". Add a name, quantity and price.");
+		
+		eventPage.enterToTextboxByRowNumberByColumnNameTicket("1", "Vip 1");
+		eventPage.enterToTextboxByRowNumberByColumnName("Quantity available", "1", "100");
+		eventPage.enterToTextboxAmountByRowNumberByColumnName("Online price", "1", "10");
+		eventPage.enterToTextboxAmountByRowNumberByColumnName("Cash price(at the door)", "1", "10");
+		eventPage.enterToTextboxAmountByRowNumberByColumnName("Day of price", "1", "10");
+		
+		eventPage.inputToTextboxMinMaxTicket("Minimum ticket", "0");
+		eventPage.clickToCommitButton("Create Ticket");
+		eventPage.sleepInSecond(5);
+		assertEquals(eventPage.getErrorMessageAtFooterPopup(), "Minimum ticket should be greater than 0.");
+		
+		eventPage.inputToTextboxMinMaxTicket("Minimum ticket", "2");
+		eventPage.inputToTextboxMinMaxTicket("Maximum ticket", "0");
+		eventPage.clickToCommitButton("Create Ticket");
+		eventPage.sleepInSecond(5);
+		assertEquals(eventPage.getErrorMessageAtFooterPopup(), "Maximum ticket should be greater than 0.");
+		
+		eventPage.inputToTextboxMinMaxTicket("Maximum ticket", "1");
+		eventPage.clickToCommitButton("Create Ticket");
+		eventPage.sleepInSecond(5);
+		assertEquals(eventPage.getErrorMessageAtFooterPopup(), "Maximum ticket should be equal or greater than Minimum ticket.");
+
+	}
+	
+	
+	@Description("Create Ticket - Success")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 3)
+	public void CreateTicket_002_CreateTicketSuccess() {
+		eventPage.inputToTextboxMinMaxTicket("Minimum ticket", "1");
+		eventPage.inputToTextboxMinMaxTicket("Maximum ticket", "10");
+		
+		eventPage.inputToTextboxMinMaxTicket("Event Name", eventName);
+		
+		eventPage.clickToCheckboxOnSale();
+		
+		eventPage.clickToCommitButton("Create Ticket");
+		
+		assertTrue(eventPage.isNameOfEventAtCalendarDisplayed(eventName));
+
+
+	}
+	
+	
+	@Description("Edit Ticket - Fail")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 4)
+	public void EditTicket_001_EditTicketFail() {
+		eventPage.clickToEvent(eventName);
+		
+		assertTrue(eventPage.isNameOfPopupDisplayed("Edit Ticket"));
+
+		eventPage.enterToTextboxByRowNumberByColumnNameTicket("1", " ");
+		eventPage.enterToTextboxByRowNumberByColumnName("Quantity available", "1", " ");
+		
+		eventPage.clickToCommitButton("Save Ticket");
+
+		assertEquals(eventPage.getErrorMessageAtFooterPopup(), "Please create a ticket level by clicking \"Add ticket\". Add a name, quantity and price.");
+
+		eventPage.clickToLink("Cancel");
+	}
+	
+	
+	
+	@Description("Edit Ticket - Success")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 5)
+	public void EditTicket_002_EditTicketSuccess() {
+		eventPage.clickToEvent(eventName);
+
+		assertTrue(eventPage.isNameOfPopupDisplayed("Edit Ticket"));
+		eventPage.clickToAddButton("Add Ticket");
+		
+		eventPage.enterToTextboxByRowNumberByColumnNameTicket("2", "Vip 2");
+		eventPage.enterToTextboxByRowNumberByColumnName("Quantity available", "1", "100");
+		
+		eventPage.clickToCommitButton("Save Ticket");
+		
+		assertTrue(eventPage.isNameOfEventAtCalendarDisplayed(eventName));
+		
+	}
+	
+	
+	
+	@Description("Copy Ticket - Fail")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 6)
+	public void CopyTicket_001_CopyTicketFail() {
+		eventPage.refreshToPage(driver);
+		eventPage.clickToEvent(eventName);
+		assertTrue(eventPage.isNameOfPopupDisplayed("Edit Ticket"));
+		
+		eventPage.clickToButtonFooter("Copy ticket");
+		assertTrue(eventPage.isNameOfPopupDisplayed("Copy Ticket"));
+
+		eventPage.clickToButtonFooter("cancel");
+
+	}
+	
+	
+	
+	@Description("Copy Ticket - Success")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 7)
+	public void CopyTicket_002_CopyTicketSuccess() {
+		eventPage.refreshToPage(driver);
+
+		eventPage.clickToEvent(eventName);
+		assertTrue(eventPage.isNameOfPopupDisplayed("Edit Ticket"));
+		
+		eventPage.clickToButtonFooter("Copy ticket");
+		assertTrue(eventPage.isNameOfPopupDisplayed("Copy Ticket"));
+		
+		eventPage.inputValueTimesDay("Choose new day:", dayStartCopy);
+		
+		eventPage.clickToUpdateButton("Copy");
+
+		assertTrue(eventPage.isNameOfEventCopyAtCalendarDisplayed(eventName));
+
+	}
+	
+	
+	
+	@Description("Transfer Ticket - Fail")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 8)
+	public void TransferTicket_001_TransferTicketFail() {
+		eventPage.refreshToPage(driver);
+
+		eventPage.clickToEvent(eventName);
+		assertTrue(eventPage.isNameOfPopupDisplayed("Edit Ticket"));
+		
+		eventPage.clickToButtonFooter("Transfer ticket");
+		assertTrue(eventPage.isNameOfPopupDisplayed("Transfer Ticket"));
+		
+		eventPage.inputValueTimesDay("Day start", dayStart);
+		eventPage.inputValueTimesDay("Day end", dayEndInValid);
+		eventPage.clickToCommitButton("Transfer Ticket");
+		
+		eventPage.acceptAlert(driver);
+		assertEquals(eventPage.getErrorMessageAtFooterPopupTransfer(), "Please choose an end date that is after the start date.");
+		
+		eventPage.clickToButtonFooter("cancel");
+
+		
+	}
+	
+	@Description("Transfer Ticket - Success")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 9)
+	public void TransferTicket_002_TransferTicketSuccess() {
+		eventPage.refreshToPage(driver);
+		eventPage.clickToEvent(eventName);
+		assertTrue(eventPage.isNameOfPopupDisplayed("Edit Ticket"));
+		
+		eventPage.clickToButtonFooter("Transfer ticket");
+		assertTrue(eventPage.isNameOfPopupDisplayed("Transfer Ticket"));
+		
+		eventPage.inputValueTimesDay("Day start", dayStartAfter);
+		eventPage.inputValueTimesDay("Day end", dayEndAfter);
+		
+		eventPage.clickToCommitButton("Transfer Ticket");
+		
+		eventPage.acceptAlert(driver);
+	}
+	
+	@Description("DeleteTicket - Fail")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 10)
+	public void DeleteTicket_001_DeleteTicketFail() {
+		eventPage.refreshToPage(driver);
+		eventPage.clickToEventCopy(eventName);
+		assertTrue(eventPage.isNameOfPopupDisplayed("Edit Ticket"));
+		eventPage.clickToButtonFooter("Delete");
+		assertTrue(eventPage.isNameOfPopupDisplayed("Delete Ticket?"));
+
+		eventPage.clickClosePopupButton();
+	}
+	
+	
+	@Description("DeleteTicket - Success")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 11)
+	public void DeleteTicket_002_DeleteTicketSuccess() {
+		eventPage.refreshToPage(driver);
+		eventPage.clickToEventCopy(eventName);
+		assertTrue(eventPage.isNameOfPopupDisplayed("Edit Ticket"));
+		eventPage.clickToButtonFooter("Delete");
+		assertTrue(eventPage.isNameOfPopupDisplayed("Delete Ticket?"));
+		
+		eventPage.clickToLink("Yes");
+	}
+	
+	
+	@Description("Cancel Refund Ticket - Refund Auto")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 12)
+	public void CancelRefundTicket_001_CancelRefundAuto() {
+		eventPage.refreshToPage(driver);
+
+		eventPage.clickToEvent(eventName);
+		assertTrue(eventPage.isNameOfPopupDisplayed("Edit Ticket"));
+		
+		String PageEvent = driver.getWindowHandle();
+		eventPage.clickToLink("Preview");		
+		eventPage.switchToWindowByID(PageEvent);
+
+		eventPage.clickToDropDownSelectQuantityTicket(ticketName, quantity);
+		eventPage.clickToAgreeCheckoutButton();
+		
+		eventPage.inputInfoBuyerTextbox("Full Name", fullName);
+		eventPage.inputInfoBuyerTextbox("Phone", phone);
+		eventPage.inputInfoBuyerTextbox("Email", validEmail);
+		eventPage.inputInfoBuyerTextbox("Confirm Email", validEmail);
+
+		eventPage.clickCheckboxAcceptTermsService();
+		
+		eventPage.getTextTotalAmountOrder();
+		eventPage.sleepInSecond(3);
+		
+		if ((eventPage.getTextTotalAmountOrder()).equals("0$")) {
+			eventPage.clickPlaceOrderButton();
+			assertTrue(eventPage.isCheckoutSuccessTextDisplayed());
+
+			String PageOrder = driver.getWindowHandle();
+			driver.close();
+			eventPage.switchToWindowByID(PageOrder);
+			
+		} else {
+
+			eventPage.switchToFrameIframe();
+			eventPage.inputInfoCardManual("Card number", cardNumberValid);
+			eventPage.inputInfoCardManual("MM / YY", monthYearValid);
+			eventPage.inputInfoCardManual("CVC", cvc);
+			eventPage.inputInfoCardManual("ZIP", zip);
+			eventPage.switchToDefaultContent();
+
+			eventPage.clickPlaceOrderButton();
+			
+			assertTrue(eventPage.isCheckoutSuccessTextDisplayed());
+						
+			String PageOrder = driver.getWindowHandle();
+			driver.close();
+			eventPage.switchToWindowByID(PageOrder);
+			
+		}
+		
+		eventPage.clickToLink("Cancel");		
+
+		eventPage.clickToEvent(eventName);
+		
+		eventPage.clickToButtonFooter("Cancel event/Refund tickets");
+
+		eventPage.acceptAlert(driver);
+		
+		
+		eventPage.clickToRadioButton("auto");
+		
+		eventPage.clickToRefundButton("Refund Tickets");
+		
+		eventPage.clickToRefundButton("Refund");
+		
+		eventPage.clickClosePopupButton();
+
+	}
+	
+	
+	
+	@Description("Cancel Refund Ticket - Manual Auto")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 13)
+	public void CancelRefundTicket_002_CancelRefundManual() {
+		eventPage.refreshToPage(driver);
+
+		
+		eventPage.clickToCreateTicketButton();
+		eventPage.inputValueTimesDay("Day start", dayStart);
+		eventPage.inputValueTimesDay("Day end", dayEnd);
+		eventPage.inputToTextboxMinMaxTicket("Event Name", eventNameNew);
+		eventPage.clickToAddButton("Add Ticket");				
+		eventPage.enterToTextboxByRowNumberByColumnNameTicket("1", "Vip 1");
+		eventPage.enterToTextboxByRowNumberByColumnName("Quantity available", "1", "100");
+		eventPage.enterToTextboxAmountByRowNumberByColumnName("Online price", "1", "10");
+		eventPage.enterToTextboxAmountByRowNumberByColumnName("Cash price(at the door)", "1", "10");
+		eventPage.enterToTextboxAmountByRowNumberByColumnName("Day of price", "1", "10");
+		eventPage.clickToCheckboxOnSale();
+		eventPage.clickToCommitButton("Create Ticket");
+		eventPage.sleepInSecond(5);
+		eventPage.refreshToPage(driver);
+
+		eventPage.clickToEvent(eventNameNew);
+		assertTrue(eventPage.isNameOfPopupDisplayed("Edit Ticket"));
+		
+		String PageEvent = driver.getWindowHandle();
+		eventPage.clickToLink("Preview");		
+		eventPage.switchToWindowByID(PageEvent);
+
+		eventPage.clickToDropDownSelectQuantityTicket(ticketName, quantity);
+		eventPage.clickToAgreeCheckoutButton();
+		
+		eventPage.inputInfoBuyerTextbox("Full Name", fullName);
+		eventPage.inputInfoBuyerTextbox("Phone", phone);
+		eventPage.inputInfoBuyerTextbox("Email", validEmail);
+		eventPage.inputInfoBuyerTextbox("Confirm Email", validEmail);
+
+		eventPage.clickCheckboxAcceptTermsService();
+		
+		eventPage.getTextTotalAmountOrder();
+		eventPage.sleepInSecond(3);
+		
+		if ((eventPage.getTextTotalAmountOrder()).equals("0$")) {
+			eventPage.clickPlaceOrderButton();
+			assertTrue(eventPage.isCheckoutSuccessTextDisplayed());
+
+			String PageOrder = driver.getWindowHandle();
+			driver.close();
+			eventPage.switchToWindowByID(PageOrder);
+			
+		} else {
+
+			eventPage.switchToFrameIframe();
+			eventPage.inputInfoCardManual("Card number", cardNumberValid);
+			eventPage.inputInfoCardManual("MM / YY", monthYearValid);
+			eventPage.inputInfoCardManual("CVC", cvc);
+			eventPage.inputInfoCardManual("ZIP", zip);
+			eventPage.switchToDefaultContent();
+
+			eventPage.clickPlaceOrderButton();
+			
+			assertTrue(eventPage.isCheckoutSuccessTextDisplayed());
+						
+			String PageOrder = driver.getWindowHandle();
+			driver.close();
+			eventPage.switchToWindowByID(PageOrder);
+			
+		}
+		
+		eventPage.clickToLink("Cancel");		
+
+		eventPage.clickToEvent(eventNameNew);
+		
+		eventPage.clickToButtonFooter("Cancel event/Refund tickets");
+
+		eventPage.acceptAlert(driver);
+		
+		
+		eventPage.clickToRadioButton("manual");
+		
+		eventPage.clickToRefundButton("Refund Tickets");
+		
+	}
+	
+	public int generateFakeNumber() {
+		Random rand = new Random();
+		return rand.nextInt(999);
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		closeBrowserDriver();
+	}
+}
+
---
+++ src/test/java/showslinger_user/Class_001_SignUp_001_AccVenue.java
@@ -0,0 +1,287 @@
+package showslinger_user;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+import commons.ConfigOnOffTestcase;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import commons.BaseTest;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import pageObject.user.UserHomePageObject;
+import pageObject.user.UserRegisterVenuePageObject;
+import utilities.ExcelHelper;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertFalse;
+
+
+public class Class_001_SignUp_001_AccVenue extends BaseTest{
+	private WebDriver driver;
+	private UserHomePageObject homePage  ;
+	private UserRegisterVenuePageObject registerVenuePage;
+	
+	private String nameOfVenue, validEmail, validPassword, existingEmail, invalidEmail, invalidPasssword;
+	private String venueName, streetAddress, townCity, validZipCode, invalidZipCode; 
+	
+	private ExcelHelper excelHelper;
+
+	@Parameters({ "envName", "serverName", "browser", "ipAddress", "portNumber", "osName", "osVersion"})
+	@BeforeClass
+	public void beforeClass(@Optional("local") String envName, @Optional("dev") String serverName,
+	@Optional("chrome") String browserName, @Optional("localhost") String ipAddress, @Optional("4444") String
+	portNumber,@Optional("Windows") String osName, @Optional("10") String osVersion)  throws Exception {
+		driver = getBrowserDriver(envName, serverName, browserName, ipAddress, portNumber, osName, osVersion);
+		homePage = new UserHomePageObject(driver);
+		excelHelper = new ExcelHelper();
+		excelHelper.setExcelFile("src/test/resources/DataTest/SignUp.xlsx", "Data");
+
+		//nameOfVenue = "Dang Giang "+ generateFakeNumberDateTime();
+		nameOfVenue = excelHelper.getCellData("nameVenue", 6) + " " + generateFakeNumberDateTime();
+		validEmail = excelHelper.getCellData("Email", 1) +"+"+ generateFakeNumberDateTime() + "@mobilefolk.com";
+		validPassword = excelHelper.getCellData("Password", 1);
+		existingEmail =  excelHelper.getCellData("Email", 2);
+		invalidEmail =  excelHelper.getCellData("Email", 3);
+		invalidPasssword =  excelHelper.getCellData("Password", 3);
+		
+		venueName = nameOfVenue; 
+		streetAddress= excelHelper.getCellData("streetAddress", 4);
+		townCity= excelHelper.getCellData("townCity", 4);
+		validZipCode= excelHelper.getCellData("ZipCode", 4);
+		invalidZipCode = excelHelper.getCellData("ZipCode", 5);
+	}
+    
+	@Description("Call form register account Venue")
+	@Severity(SeverityLevel.CRITICAL)
+	@Test(priority = 1)
+	public void AccVenue_001_FormRegister() {
+		if(ConfigOnOffTestcase.isReCapTChaOn){
+			homePage.clickToRegisterLink();
+			registerVenuePage = new UserRegisterVenuePageObject(driver);
+			registerVenuePage.clickToCallFormResgiter();
+			registerVenuePage.clickToRadioButtonVenue();
+		} else{
+			System.out.println("Not Handle RecapTCha");
+		}
+
+	}
+
+	@Description("Register account Venue -  when empty data")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 2)
+	public void AccVenue_002_RegisterEmptyData() throws Exception{
+		if(ConfigOnOffTestcase.isReCapTChaOn){
+			excelHelper.setExcelFile("src/test/resources/DataTest/SignUp.xlsx", "Message");
+			registerVenuePage.clickToRegisterButton();
+			assertEquals(registerVenuePage.getErrorMessageAtEmailTextbox(),excelHelper.getCellData("Message", 2));
+			assertEquals(registerVenuePage.getErrorMessageAtPasswordTextbox(),excelHelper.getCellData("Message", 3));
+			assertEquals(registerVenuePage.getErrorMessageAtConfirmPasswordTextbox(),excelHelper.getCellData("Message", 4));
+		} else{
+			System.out.println("Not Handle RecapTCha");
+		}
+
+	}
+	
+	
+	@Description("Register account Venue - Don't verify ReCAPTCHA")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 3)
+	public void AccVenue_003_RegisterNotVerifyReCAPTCHA() throws Exception{
+		if(ConfigOnOffTestcase.isReCapTChaOn){
+			excelHelper.setExcelFile("src/test/resources/DataTest/SignUp.xlsx", "Message");
+			registerVenuePage.inputToTextbox("Name", nameOfVenue);
+			registerVenuePage.inputToTextbox("Email",validEmail);
+			registerVenuePage.inputToTextbox("Password",validPassword);
+			registerVenuePage.clickCheckboxAcceptTerms();
+			registerVenuePage.clickToRegisterButton();
+			assertEquals(registerVenuePage.getErrorMessageAtHeader(),excelHelper.getCellData("Message", 5));
+		} else{
+			System.out.println("Not Handle RecapTCha");
+		}
+
+	}
+	
+	@Description("Register account Venue - input email already exists")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 4)
+	public void AccVenue_004_RegisterEmailAlredyExists() throws Exception{
+		if(ConfigOnOffTestcase.isReCapTChaOn){
+			excelHelper.setExcelFile("src/test/resources/DataTest/SignUp.xlsx", "Message");
+			registerVenuePage.clickToCallFormResgiter();
+			registerVenuePage.clickToRadioButtonVenue();
+
+			registerVenuePage.inputToTextbox("Name", nameOfVenue);
+			registerVenuePage.inputToTextbox("Email",existingEmail);
+			registerVenuePage.inputToTextbox("Password",validPassword);
+
+//		registerVenuePage.switchToFrameIframe();
+//		registerVenuePage.clickCheckboxNotRobot();
+//		registerVenuePage.switchToSignInPage();
+
+			//manual verify reCAPTCHA
+			registerVenuePage.sleepInSecond(10);
+
+			registerVenuePage.clickToRegisterButton();
+
+			assertEquals(registerVenuePage.getErrorMessageAtHeader(),excelHelper.getCellData("Message", 6));
+		} else{
+			System.out.println("Not Handle RecapTCha");
+		}
+
+
+	}
+	
+	@Description("Register account Venue - input email is wrong")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 5)
+	public void AccVenue_005_RegisterEmailWrong() throws Exception{
+		if(ConfigOnOffTestcase.isReCapTChaOn){
+			excelHelper.setExcelFile("src/test/resources/DataTest/SignUp.xlsx", "Message");
+			registerVenuePage.clickToCallFormResgiter();
+			registerVenuePage.clickToRadioButtonVenue();
+
+			registerVenuePage.inputToTextbox("Name", nameOfVenue);
+			registerVenuePage.inputToTextbox("Email",invalidEmail);
+			registerVenuePage.inputToTextbox("Password",validPassword);
+
+//		registerVenuePage.clickCheckboxAcceptTerms();
+
+//		registerVenuePage.switchToFrameIframe();
+//		registerVenuePage.clickCheckboxNotRobot();
+//		registerVenuePage.switchToSignInPage();
+
+			//manual verify reCAPTCHA
+			registerVenuePage.sleepInSecond(10);
+
+			registerVenuePage.clickToRegisterButton();
+			assertEquals(registerVenuePage.getErrorMessageAtHeader(),excelHelper.getCellData("Message", 7));
+		} else{
+			System.out.println("Not Handle RecapTCha");
+		}
+
+	}
+	
+	
+	@Description("Register account Venue Profile - Empty data")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 6)
+	public void AccVenue_006_RegisterVenueProfileEmpty() throws Exception{
+		if(ConfigOnOffTestcase.isReCapTChaOn){
+			excelHelper.setExcelFile("src/test/resources/DataTest/SignUp.xlsx", "Message");
+			registerVenuePage.clickToCallFormResgiter();
+			registerVenuePage.clickToRadioButtonVenue();
+
+			registerVenuePage.inputToTextbox("Name", nameOfVenue);
+			registerVenuePage.inputToTextbox("Email",validEmail);
+			registerVenuePage.inputToTextbox("Password",validPassword);
+
+//		registerVenuePage.clickCheckboxAcceptTerms();
+
+//		registerVenuePage.switchToFrameIframe();
+//		registerVenuePage.clickCheckboxNotRobot();
+//		registerVenuePage.switchToSignInPage();
+
+			//manual verify reCAPTCHA
+			registerVenuePage.sleepInSecond(10);
+
+			registerVenuePage.clickToRegisterButton();
+
+			assertTrue(registerVenuePage.isTextCreateYourVenueProfile());
+			registerVenuePage.clickToButtonCreateVenue();
+
+			assertEquals(registerVenuePage.getErrorMessageAtField("Venue Name"),excelHelper.getCellData("Message", 8));
+			assertEquals(registerVenuePage.getErrorMessageAtField("Street Address 1"),excelHelper.getCellData("Message", 9));
+			assertEquals(registerVenuePage.getErrorMessageAtField("Town/City"),excelHelper.getCellData("Message", 10));
+			assertEquals(registerVenuePage.getErrorMessageAtField("Zip/Postal Code"),excelHelper.getCellData("Message", 11));
+
+		} else{
+			System.out.println("Not Handle RecapTCha");
+		}
+
+	}
+	
+	@Description("Register account Venue Profile - zip code is wrong")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 7)
+	public void AccVenue_007_RegisterVenueProfileZipCodeWrong() throws Exception{
+		if(ConfigOnOffTestcase.isReCapTChaOn){
+			excelHelper.setExcelFile("src/test/resources/DataTest/SignUp.xlsx", "Message");
+			registerVenuePage.inputToTextboxProfile("Venue Name", venueName);
+			registerVenuePage.inputToTextboxProfile("Street Address 1",streetAddress);
+			registerVenuePage.inputToTextboxProfile("Town/City",townCity);
+			registerVenuePage.inputToTextboxProfile("Zip/Postal Code",invalidZipCode);
+
+			registerVenuePage.clickToButtonCreateVenue();
+
+			assertEquals(registerVenuePage.getErrorMessageAtField("Zip/Postal Code"),excelHelper.getCellData("Message", 12));
+		} else{
+			System.out.println("Not Handle RecapTCha");
+		}
+	}
+	
+	@Description("Register account Venue Profile - Photo empty data")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 8)
+	public void AccVenue_008_RegisterVenueAddPhotoEmpty() throws Exception{
+		if(ConfigOnOffTestcase.isReCapTChaOn){
+			excelHelper.setExcelFile("src/test/resources/DataTest/SignUp.xlsx", "Message");
+			registerVenuePage.inputToTextboxProfile("Venue Name", venueName);
+			registerVenuePage.inputToTextboxProfile("Street Address 1",streetAddress);
+			registerVenuePage.inputToTextboxProfile("Town/City",townCity);
+			registerVenuePage.inputToTextboxProfile("Zip/Postal Code",validZipCode);
+
+			registerVenuePage.clickToButtonCreateVenue();
+
+			assertTrue(registerVenuePage.isTextAddPhoto());
+			registerVenuePage.clickToButtonUpload();
+
+			assertEquals(registerVenuePage.getErrorMessageUploadPhoto(),excelHelper.getCellData("Message", 13));
+		} else{
+			System.out.println("Not Handle RecapTCha");
+		}
+
+	}
+	
+	
+	@Description("Register account Venue Profile - register complelet - Skip upload photo")
+	@Severity(SeverityLevel.NORMAL)
+	@Test(priority = 9)
+	public void AccVenue_009_RegisterCompteledSkipUploadImage() throws Exception{
+		if(ConfigOnOffTestcase.isReCapTChaOn){
+			excelHelper.setExcelFile("src/test/resources/DataTest/SignUp.xlsx", "Message");
+			registerVenuePage.clickToButtonSkip();
+			registerVenuePage.clickShowLeftMenu();
+			assertTrue(registerVenuePage.isTextTicketing());
+		} else{
+			System.out.println("Not Handle RecapTCha");
+		}
+
+	}
+	
+	public int generateFakeNumber() {
+		Random rand = new Random();
+		return rand.nextInt(9999);
+	}
+
+	public static long generateFakeNumberDateTime() {
+		LocalDateTime now = LocalDateTime.now();
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+		String formattedDateTime = now.format(formatter);
+
+		return Long.parseLong(formattedDateTime);
+	}
+
+	@AfterClass
+	public void afterClass() {
+		closeBrowserDriver();
+	}
+}
